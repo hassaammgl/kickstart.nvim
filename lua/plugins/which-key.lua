3a5b7003b91f2abb6ace51c3706bbb90f9f2d993
hassaammgl <h455a4mmehtab@gmail.com>
2026-01-04 21:15:27 +0500

    Add initial configuration files and plugins for LazyVim setup

---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,94 @@
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts_extend = { "spec" },
+  opts = {
+    preset = "helix",
+    defaults = {},
+    spec = {
+      {
+        mode = { "n", "x" },
+        { "<leader><tab>", group = "tabs" },
+        { "<leader>c", group = "code" },
+        { "<leader>d", group = "debug" },
+        { "<leader>dp", group = "profiler" },
+        { "<leader>f", group = "file/find" },
+        { "<leader>g", group = "git" },
+        { "<leader>gh", group = "hunks" },
+        { "<leader>q", group = "quit/session" },
+        { "<leader>s", group = "search" },
+        { "<leader>u", group = "ui" },
+        { "<leader>x", group = "diagnostics/quickfix" },
+        { "[", group = "prev" },
+        { "]", group = "next" },
+        { "g", group = "goto" },
+        { "gs", group = "surround" },
+        { "z", group = "fold" },
+        {
+          "<leader>b",
+          group = "buffer",
+          expand = function()
+            return require("which-key.extras").expand.buf()
+          end,
+        },
+        {
+          "<leader>w",
+          group = "windows",
+          proxy = "<c-w>",
+          expand = function()
+            return require("which-key.extras").expand.win()
+          end,
+        },
+        -- better descriptions
+        { "gx", desc = "Open with system app" },
+      },
+    },
+  },
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer Keymaps (which-key)",
+    },
+    {
+      "<c-w><space>",
+      function()
+        require("which-key").show({ keys = "<c-w>", loop = true })
+      end,
+      desc = "Window Hydra Mode (which-key)",
+    },
+  },
+  window = {
+    border = "rounded",      -- none | single | double | shadow | rounded
+    position = "bottom",     -- bottom | top
+    margin = { 1, 0, 1, 0 }, -- top, right, bottom, left
+    padding = { 1, 2, 1, 2 },
+    winblend = 0,            -- transparency (0–100)
+  },
+
+  layout = {
+    height = { min = 4, max = 25 },
+    width  = { min = 20, max = 50 },
+    spacing = 3,
+    align = "left",
+  },
+
+  icons = {
+    breadcrumb = "»",
+    separator = "➜",
+    group = "+",
+  },
+
+  show_help = true,
+  show_keys = true,
+  config = function(_, opts)
+    local wk = require("which-key")
+    wk.setup(opts)
+    if not vim.tbl_isempty(opts.defaults) then
+      LazyVim.warn("which-key: opts.defaults is deprecated. Please use opts.spec instead.")
+      wk.register(opts.defaults)
+    end
+  end,
+}
